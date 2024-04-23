f081ad2776ebec2cb4fad96d4080b94027579f69
Edir P Santos <fennder@gmail.com>
2024-04-23 11:48:14 -0300

    Include getDeleteString

--- src/main/java/br/dev/abrindoportas/WebApp/controller/TaskController.java
+++ src/main/java/br/dev/abrindoportas/WebApp/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -60,4 +61,10 @@ public class TaskController {
         return mv;
     }
 
+    @GetMapping("/delete/{id}")
+    public String getDeleteString(@PathVariable("id") Long id){
+        taskModels.removeIf(taskModel -> id.equals(taskModel.getId()));
+        return "redirect:/list";
+    }
+    
 }
